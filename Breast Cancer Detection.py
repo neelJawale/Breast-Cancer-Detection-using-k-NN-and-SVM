15cf9b68ff98b6407bdbd6bd2e8ddebd588d1c36
Neel Jawale <68111801+ziggyStardust-01@users.noreply.github.com>
2022-12-29 16:19:37 -0800

    Updated Code with comments.

--- Breast Cancer Detection.py
+++ Breast Cancer Detection.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[115]:
-
-
 import sys
 import scipy
 import numpy
 import matplotlib
 import pandas
 import sklearn
-
-
-# In[116]:
-
-
 import numpy as np
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -27,10 +19,6 @@ from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-# In[117]:
-
-
 # Load the dataset and create a dataframe.
 data = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
 features = ['id', 'clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
@@ -40,64 +28,31 @@ df = pd.read_csv(data, names=features)
 print(df)
 
 
-# In[118]:
-
-
 # Preprocess the dataset
 df.replace('?',-99999, inplace=True) # Replace the missing or invalid data with a placeholder value to keep data consistent.
 print(df.axes)
 df.drop(['id'], axis=1, inplace=True)
 
-
-# In[119]:
-
-
 # Explore the nature of the data
 print(df.loc[698])
 print(df.shape)
-
-
-# In[120]:
-
-
 print(df.describe())
-
-
-# In[121]:
-
 
 # Histogram Visualization of the data
 df.hist(figsize = (10, 10))
 plt.show()
 
-
-# In[122]:
-
-
 # To see the inter-dependencies of the variables, use scatter plot
 scatter_matrix(df, figsize = (18,18))
 plt.show()
 
-
-# In[123]:
-
-
 # Create X and Y datasets for training
 X = np.array(df.drop(['class'],axis=1))
 y = np.array(df['class'])
-
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-
-
-# In[124]:
-
 
 seed = 12 # Define seed for reproducibility of the results during validation
 scoring = 'accuracy'
-
-
-# In[125]:
-
 
 # Define models to train
 models = []
@@ -116,10 +71,6 @@ for name, model in models:
     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
     print(msg)
 
-
-# In[129]:
-
-
 # Make predictions on test/validation dataset
 for name, model in models:
     model.fit(X_train, y_train)
@@ -129,12 +80,7 @@ for name, model in models:
     print(classification_report(y_test, predictions))
 print(predictions)
 
-
-# In[127]:
-
-
 clf = SVC()
-
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 print(accuracy)
